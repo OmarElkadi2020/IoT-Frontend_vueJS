4bb361f66bc204f342852b368655a39fdebdee5f
Omar Elkadi <omar.elkadi@stud.th-deg.de>
2021-04-22 12:38:06 +0200

    Routing/Bootstrap/sass

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//add routing script to the main
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
